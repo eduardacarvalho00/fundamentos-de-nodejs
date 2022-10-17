16670bedac4645295d7f94eca66c452ebb16b24c
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2022-10-17 12:38:56 -0300

    feat: delete account / search by date

--- src/index.js
+++ src/index.js
@@ -20,6 +20,18 @@ function verifyExistsAccountCPF(request, response, next) {
   return next();
 };
 
+function getBalance(statement){ // transforma os valores em um unico valor
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit'){
+      return acc + operation.amount;
+    }else{
+      return acc - operation.amount;
+    }
+  }, 0);
+  
+  return balance;
+}
+
 app.post('/user', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -64,4 +76,60 @@ app.post('/deposit', verifyExistsAccountCPF,(request, response) => {
   return response.status(201).send()
 });
 
+app.post('/withdraw', verifyExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request; 
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount){
+    return response.status(400).json({ error: "Insuffcient fouds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send()
+});
+
+app.get('/statement/date', verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat =  new Date(date + " 00:00")
+
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return response.json(statement)
+});
+
+app.put('/account', verifyExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+})
+
+app.get('/account', verifyExistsAccountCPF, (request, response) => {
+ const { customer } = request;
+
+ return response.json(customer);
+})
+
+app.delete('/account', verifyExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customers.indexOf(customer),1)
+
+  return response.status(200).json(customers);
+})
+
 app.listen(3000);
+
+
